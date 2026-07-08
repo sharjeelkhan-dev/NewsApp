55a5a443f9c0e7beffda6d6a1cfbc158ca9de6b2
Sharjeel Khan <sharjeelkhan111213@gmail.com>
2026-07-08 22:04:32 +0500

    Latest Update 08/07/2026

--- app/src/main/java/com/sharjeel/newsapp/ui/screens/onboarding/SplashScreen.kt
+++ app/src/main/java/com/sharjeel/newsapp/ui/screens/onboarding/SplashScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -150,8 +148,6 @@ fun AdvancedSplashScreen(
                     )
                 )
             }
-
-            Spacer(modifier = Modifier.height(32.dp))
 
             Text(
                 text = "TRUTH IN FOCUS",
