396313199e99be4179e2ac4cd65ad64f0c807a39
Sharjeel Khan <sharjeelkhan111213@gmail.com>
2026-07-08 21:56:20 +0500

    Latest Update 08/07/2026

--- app/src/main/java/com/sharjeel/newsapp/ui/screens/details/DetailViewModel.kt
+++ app/src/main/java/com/sharjeel/newsapp/ui/screens/details/DetailViewModel.kt
@@ -3,11 +3,9 @@ package com.sharjeel.newsapp.ui.screens.details
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.sharjeel.newsapp.domain.model.Article
 import com.sharjeel.newsapp.domain.repository.NewsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
